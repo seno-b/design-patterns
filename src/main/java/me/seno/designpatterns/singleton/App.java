5c359153175e31908f05fa53bd7b41fd4320eada
senob <bangshinho@gmail.com>
2021-12-28 23:51:38 +0900

    feat: 객체 생성 관련 디자인 패턴 싱글톤 패턴 3부 - 싱글톤 패턴 구현 방법을 깨뜨리는 방법

--- src/main/java/me/seno/designpatterns/singleton/App.java
+++ src/main/java/me/seno/designpatterns/singleton/App.java
@@ -1,9 +1,15 @@
 package me.seno.designpatterns.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Settings settings = Settings.getInstance();
-        Settings settings1 = Settings.getInstance();
+
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings1 = constructor.newInstance();
 
         System.out.println(settings == settings1);
     }
